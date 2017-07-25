3554ae1aa290e2ce9aa7cee5c09c31a6dfa8e298
Keith Evans <kwlevans@gmail.com>
2017-07-25 16:42:39 -0700

    add initial field structure

--- sites/all/themes/ddrupal_themes/views-view-fields--race-select--page.tpl.php
+++ sites/all/themes/ddrupal_themes/views-view-fields--race-select--page.tpl.php
@@ -1,3 +1,11 @@
 <?php
 
 dpm($fields);
+?>
+<div class='race-wrapper'>
+  <h2><?php print($fields['title']->content)?></h2>
+  <?php print($fields['field_race_image']->content)?>
+  <div class="race-flavor-text">
+    <?php print($fields['field_race_flavor_text']->content)?>
+  </div>
+</div>
