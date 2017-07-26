06a98e34008ceb95844ab0f697ba63e52faa7f31
Keith Evans <kwlevans@gmail.com>
2017-07-26 15:44:18 -0700

    debug

--- sites/all/modules/ddrupal/js/scripts.js
+++ sites/all/modules/ddrupal/js/scripts.js
@@ -49,8 +49,7 @@
       //Select controls for race select page
       if (window.location.pathname === '/new_character/race') {
         $('.race-wrapper').once('race-select-click').click(function() {
-          $(this).children('input').trigger('click');
-          $('#set-race-button').removeProp('disabled');
+          console.log($(this).children('input'));
         });
       }
 
