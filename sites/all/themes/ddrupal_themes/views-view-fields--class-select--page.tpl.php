ab02476fd66fc67e3146a8b09505de56370c82f8
Keith Evans <kwlevans@gmail.com>
2017-07-26 20:43:24 -0700

    add class select page

--- sites/all/themes/ddrupal_themes/views-view-fields--class-select--page.tpl.php
+++ sites/all/themes/ddrupal_themes/views-view-fields--class-select--page.tpl.php
@@ -4,8 +4,8 @@
 <div class='select-wrapper'>
   <input type="radio" value="<?php print($fields['nid']->raw) ?>" hidden>
   <h2><?php print($fields['title']->content)?></h2>
-  <?php print($fields['field_race_image']->content)?>
+  <?php print($fields['field_class_image']->content)?>
   <div class="select-flavor-text">
-    <?php print($fields['field_race_flavor_text']->content)?>
+    <?php print($fields['field_class_flavor_text']->content)?>
   </div>
 </div>
