88112af79700d348b52742e5a095315ec99e5f8c
Keith Evans <kwlevans@gmail.com>
2017-07-27 11:01:15 -0700

    building select carousel

--- sites/all/themes/ddrupal_themes/views-view-fields--class-select--page.tpl.php
+++ sites/all/themes/ddrupal_themes/views-view-fields--class-select--page.tpl.php
@@ -3,8 +3,10 @@
 ?>
 <div class='select-wrapper'>
   <input type="radio" value="<?php print($fields['nid']->raw) ?>" hidden>
+  <div class="select-carousel">
+    <?php print($fields['field_class_image']->content)?>
+  </div>
   <h2><?php print($fields['title']->content)?></h2>
-  <?php print($fields['field_class_image']->content)?>
   <div class="select-flavor-text">
     <?php print($fields['field_class_flavor_text']->content)?>
   </div>
--- sites/all/themes/ddrupal_themes/views-view-fields--race-select--page.tpl.php
+++ sites/all/themes/ddrupal_themes/views-view-fields--race-select--page.tpl.php
@@ -3,8 +3,10 @@
 ?>
 <div class='select-wrapper'>
   <input type="radio" value="<?php print($fields['nid']->raw) ?>" hidden>
+  <div class="select-carousel">
+    <?php print($fields['field_race_image']->content)?>
+  </div>
   <h2><?php print($fields['title']->content)?></h2>
-  <?php print($fields['field_race_image']->content)?>
   <div class="select-flavor-text">
     <?php print($fields['field_race_flavor_text']->content)?>
   </div>
